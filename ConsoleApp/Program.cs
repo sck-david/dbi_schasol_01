e32ddc97f0967215640a03a4e7a96e1ffc186815
sck-david <SCH20370@spengergasse.at>
2023-11-20 12:29:18 +0100

    init project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MongoDB.Driver;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("mongodb://localhost:27017/");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
