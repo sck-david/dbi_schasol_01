824b6bdd8861af22ca3a36c9095602ef68b79120
sck-david <SCH20370@spengergasse.at>
2023-11-29 10:17:15 +0100

    added frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+  <div id="app">
+     <h1>Club Selection</h1>
+     <select v-model="selectedClubId">
+       <option v-for="club in clubs" :key="club.Id" :value="club.Id">
+         {{ club.clubName }}
+       </option>
+     </select>
+ 
+     <PlayerList :selectedClub="selectedClub" />
+  </div>
+ </template>
+ 
+ <script>
+ import PlayerList from './components/PlayerList.vue';
+ import axios from 'axios';
+ 
+ export default {
   name: 'App',
   components: {
-    HelloWorld
+     PlayerList
+  },
+  data() {
+     return {
+       clubs: [],
+       selectedClubId: null,
+       selectedClub: null
+     };
+  },
+  created() {
+     axios.get('http://localhost:5000/api/clubs')
+       .then(response => {
+         this.clubs = response.data;
+         this.selectedClubId = this.clubs[0].Id;
+         this.selectedClub = this.clubs[0];
+       });
+  },
+  watch: {
+     selectedClubId(newValue) {
+       this.selectedClub = this.clubs.find(club => club.Id === newValue);
+     }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+ }
+ </script>
---
+++ frontend/src/components/PlayerList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+       <h1>{{ selectedClub.clubName }}</h1>
+       <table>
+         <thead>
+           <tr>
+             <th>Firstname</th>
+             <th>Lastname</th>
+             <th>Date of Birth</th>
+             <th>Market Value</th>
+             <th>Gender</th>
+           </tr>
+         </thead>
+         <tbody>
+           <tr v-for="player in selectedClub.players" :key="player.Id">
+             <td>{{ player.firstname }}</td>
+             <td>{{ player.lastname }}</td>
+             <td>{{ player.gebDat }}</td>
+             <td>{{ player.marketvalue }}</td>
+             <td>{{ player.gender }}</td>
+           </tr>
+         </tbody>
+       </table>
+    </div>
+   </template>
+   
+   <script>
+   export default {
+    props: {
+       selectedClub: Object
+    }
+   }
+   </script>
